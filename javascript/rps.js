4c1069872aafdc77d68574a24f4f1de6f301e083
Pureharish <108225503+pureharish@users.noreply.github.com>
2022-12-07 01:57:21 +0530

    am.

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,35 +1,35 @@
-usr_input = prompt('R P or S?: ');
-user_input = String(usr_input);
+// usr_input = prompt('R P or S?: ');
+// user_input = String(usr_input);
 
-console.log(user_input);
-
-
-raw_computer_inputer = ['r', 'p', 's'];
-
-computer_input = raw_computer_inputer[Math.floor(Math.random() * raw_computer_inputer.length)];
-
-console.log(computer_input)
-
-computer_input = String(computer_input)
-
-function rps(computer_input, user_input) {
-    if (computer_input === user_input) {
-        console.log(`It's a draw`);
-    }
-    else if(computer_input === 'r' && user_input === 'p') {
-        console.log(`You win!`);
-    }
-    else if (computer_input === 'p' && user_input === 's') {
-        console.log('You win!');
-    }
-    else if (computer_input == 's' && user_input === 'r') {
-        console.log('You win!');
-    }
-
-    else {
-        console.log('You lose :(');
-    }
-}
+// console.log(user_input);
 
 
-rps(computer_input, user_input)
+// raw_computer_inputer = ['r', 'p', 's'];
+
+// computer_input = raw_computer_inputer[Math.floor(Math.random() * raw_computer_inputer.length)];
+
+// console.log(computer_input)
+
+// computer_input = String(computer_input)
+
+// function rps(computer_input, user_input) {
+//     if (computer_input === user_input) {
+//         console.log(`It's a draw`);
+//     }
+//     else if(computer_input === 'r' && user_input === 'p') {
+//         console.log(`You win!`);
+//     }
+//     else if (computer_input === 'p' && user_input === 's') {
+//         console.log('You win!');
+//     }
+//     else if (computer_input == 's' && user_input === 'r') {
+//         console.log('You win!');
+//     }
+
+//     else {
+//         console.log('You lose :(');
+//     }
+// }
+
+
+// rps(computer_input, user_input)
