46a9d29c91f16c7c6909e6796702c3e126e6f2f1
Pureharish <108225503+pureharish@users.noreply.github.com>
2022-12-08 02:40:28 +0530

    made the game functional

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,62 +1,71 @@
-// usr_input = prompt('R P or S?: ');
-// user_input = String(usr_input);
 
-// console.log(user_input);
+const rock = document.getElementById("btn-rock");
 
+const paper = document.getElementById("btn-paper");
 
-// raw_computer_inputer = ['r', 'p', 's'];
+const scissors = document.getElementById("btn-scissors");
 
-// computer_input = raw_computer_inputer[Math.floor(Math.random() * raw_computer_inputer.length)];
+function events(r = rock, p = paper, s = scissors) {
+  r.addEventListener("click", (e) => {
+    usr_input = "r";
+    play(usr_input);
+  });
+  p.addEventListener("click", (e) => {
+    usr_input = "p";
+    play(usr_input);
+  });
+  s.addEventListener("click", (e) => {
+    usr_input = "s";
+    play(usr_input);
+  });
+}
 
-// console.log(computer_input)
+function play(usr_input) {
+  const result = document.getElementById("mid-text-font");
 
-// computer_input = String(computer_input)
+  let raw_computer_inputer = ["r", "p", "s"];
 
-// function rps(computer_input, user_input) {
-//     if (computer_input === user_input) {
-//         console.log(`It's a draw`);
-//     }
-//     else if(computer_input === 'r' && user_input === 'p') {
-//         console.log(`You win!`);
-//     }
-//     else if (computer_input === 'p' && user_input === 's') {
-//         console.log('You win!');
-//     }
-//     else if (computer_input == 's' && user_input === 'r') {
-//         console.log('You win!');
-//     }
+  let computer_input =
+    raw_computer_inputer[
+      Math.floor(Math.random() * raw_computer_inputer.length)
+    ];
 
-//     else {
-//         console.log('You lose :(');
-//     }
-// }
+  computer_input = String(computer_input);
 
+  if (computer_input === usr_input) {
+    result.textContent = `It's a draw ${draw}`;
+  } else if (computer_input === "r" && usr_input === "p") {
+    result.textContent = `You win!`;
+  } else if (computer_input === "p" && usr_input === "s") {
+    result.textContent = "You win!";
+  } else if (computer_input == "s" && usr_input === "r") {
+    result.textContent = "You win!";
+  } else {
+    result.textContent = "You lose :(";
+  }
+}
 
-// rps(computer_input, user_input)
+const button = document.querySelectorAll(".btn");
 
-/*
-I want tp get make a span and there get the cordinates of X and Y 
-*/
+button.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
 
-const button = document.querySelectorAll('.btn')
+    const span = document.createElement("span");
+    span.classList.add("ripple");
 
-console.log(button);
+    span.style.top = `${y}px`;
+    span.style.left = `${x}px`;
 
+    this.appendChild(span);
 
- button.forEach(btn => {
-    btn.addEventListener('click', function(e){
-        let x = e.clientX - e.target.offsetLeft;
-        let y = e.clientY - e.target.offsetTop;
+    setTimeout(() => this.removeChild(span), 450);
+  });
+});
 
-        const span = document.createElement('span');
-        span.classList.add('ripple')
-
-        span.style.top = `${y}px`;
-        span.style.left = `${x}px`;
-
-        this.appendChild(span);
-
-        setTimeout(() => this.removeChild(span), 450)
-    })
-})
-
+button.forEach((bttnn) => {
+  bttnn.addEventListener("click", (e) => {
+    events();
+  });
+});
